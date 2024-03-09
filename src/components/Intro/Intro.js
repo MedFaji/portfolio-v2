cb511fa35b5b96e95cc1c2436ab0dd3882673cd2
Med Faji <meedfaji@gmail.com>
2024-03-09 23:39:43 +0100

    update intro text

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -11,13 +11,14 @@ const Intro = () => {
         <span className="hello">Hello,</span>
         <span className="intro-text">
           I'm <span className="intro-name">Mohamed Faji</span> <br /> Junior
-          Software Engineer
+          Software Developer
         </span>
         <p className="intro-para">
-          Recent Master's graduate in Information Systems Engineering and a
-          dynamic Junior Software Engineer with a passion for innovation and a
-          drive for delivering top-notch web experiences. Eager to learn and
-          committed to excellence in code. Ready to make an impact!
+          Recent Master's graduate in Information Systems Engineering and Junior
+          Software Developer with a focus on
+          <b> Java, React, and the MERN stack</b>. Passionate about innovation
+          and committed to delivering top-notch web experiences through hands-on
+          project work. Eager to make an impact through excellence in code.
         </p>
         <Link to="contact">
           <button className="btn">
@@ -26,7 +27,7 @@ const Intro = () => {
           </button>
         </Link>
       </div>
-      <img src={bg} alt="Profile" className="bg" />
+      {/* <img src={bg} alt="Profile" className="bg" /> */}
     </section>
   );
 };
